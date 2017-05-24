9ebf6559412e70de448f0158564830fe80e43d15
leannegu <leannegu@Leannes-MacBook-Pro.local>
2017-05-24 15:35:12 +1000

    reader updates

--- Reader/assets/js/reader.js
+++ Reader/assets/js/reader.js
@@ -3,32 +3,6 @@ $(document).ready(function() {
 
 // Add jQuery here
 
-
-// counter
-  var counter = document.getElementsByClassName("counter")[0];
-      var min    = 0;
-      var second = 00;
-      var zeroPlaceholder = 0;
-      var counterId = setInterval(function(){
-                        countUp();
-                      }, 1000);
-
-      function countUp () {
-          second++;
-          if(second == 59){
-            second = 00;
-            min = min + 1;
-          }
-          if(second == 10){
-              zeroPlaceholder = '';
-          }else
-          if(second == 00){
-              zeroPlaceholder = 0;
-          }
-
-          counter.innerText = min+':'+zeroPlaceholder+second;
-      }
-
 // menu spinning quesiton mark
 $('.nav').click(function() {
    $('.menu').toggleClass('showme');
