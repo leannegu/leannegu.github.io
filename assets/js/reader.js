0bbb6292476cf07efecbcdac4a994cfae418eb15
leannegu <leannegu@Leannes-MacBook-Pro.local>
2017-05-24 23:41:24 +1000

    scroll checker

--- assets/js/reader.js
+++ assets/js/reader.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 //   $('.menu').toggleClass('showme');
 // });
 
+
 //click fake link game over appears
 
   $('.dummylink').on('click', function() {
@@ -150,3 +151,46 @@ $('.oase').mouseenter(function() {
             $(this).remove();
           });
   });
+
+  // stolen scrollchecker
+  var checkScrollSpeed = (function(settings){
+      settings = settings || {};
+
+      var lastPos, newPos, timer, delta,
+          delay = settings.delay || 50; // in "ms" (higher means lower fidelity )
+
+      function clear() {
+        lastPos = null;
+        delta = 0;
+      }
+
+      clear();
+
+      return function(){
+        newPos = window.scrollY;
+        if ( lastPos != null ){ // && newPos < maxScroll
+          delta = newPos -  lastPos;
+        }
+        lastPos = newPos;
+        clearTimeout(timer);
+        timer = setTimeout(clear, delay);
+        return delta;
+      };
+  })();
+
+  // listen to "scroll" event
+  window.onscroll = function(){
+
+
+    var speed = checkScrollSpeed();
+  // if speed is faster than 30ms, adjust if needed
+
+    if (speed > 100) {
+      $('.slowdown').addClass('show');
+    }
+    if (speed < 30) {
+      $('.slowdown').removeClass('show');
+    }
+
+
+  };
