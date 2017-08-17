23f3e8920017ff69e3e9ebb2cd3e89eb80756b4e
leannegu <lgu19@student.monash.edu>
2017-08-18 09:14:54 +1000

    push

---
+++ 5posters/assets/js/poster5.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+   setTimeout(function () {
+     $('.saycheese').addClass('show');
+   }, 4000);
+
+   setTimeout(function () {
+     $('.cheese, .saycheese1, .yellowcircle, .yellowcircle2, .yellowcircle3').addClass('show');
+   }, 2000);
+
+  //  setTimeout(function () {
+  //    $('.one').removeClass('show');
+  //  }, 5000);
+  //
+  //  setTimeout(function () {
+  //    $('.two').toggleClass('show');
+  //  }, 3000);
+
+  //  setTimeout(function () {
+  //    $('.two').removeClass('show');
+  //  }, 7000);
+
+    });
