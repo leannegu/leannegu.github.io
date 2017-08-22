9fe1b366a94c76171f29560698ca80811c0ff8f2
leannegu <lgu19@student.monash.edu>
2017-08-22 19:57:16 +1000

    index

--- 5posters/assets/js/main.js
+++ 5posters/assets/js/main.js
@@ -17,27 +17,27 @@ $(document).ready(function() {
   });
 
   $('.poster3').mouseenter(function() {
-      $('.poster3italic').addClass('showme');
+      $('.poster3italic, .poster3').addClass('showme');
     });
 
     $('.poster3').mouseleave(function() {
-   $('.poster3italic').removeClass('showme');
+   $('.poster3italic, .poster3').removeClass('showme');
   });
 
   $('.poster4').mouseenter(function() {
-      $('.poster4italic').addClass('showme');
+      $('.poster4italic, .poster4').addClass('showme');
     });
 
     $('.poster4').mouseleave(function() {
-   $('.poster4italic').removeClass('showme');
+   $('.poster4italic, .poster4').removeClass('showme');
   });
 
   $('.poster5').mouseenter(function() {
-      $('.poster5italic').addClass('showme');
+      $('.poster5italic, .poster5').addClass('showme');
     });
 
     $('.poster5').mouseleave(function() {
-   $('.poster5italic').removeClass('showme');
+   $('.poster5italic, .poster5').removeClass('showme');
   });
 
     });
