8ba465f1140a1b71097fcf04618311653ef19e53
leannegu <lgu19@student.monash.edu>
2017-08-22 20:38:18 +1000

    5posters push

--- 5posters/assets/js/main.js
+++ 5posters/assets/js/main.js
@@ -1,43 +1,43 @@
 $(document).ready(function() {
   $('.poster1').mouseenter(function() {
-      $('.poster1italic, .poster1').addClass('showme');
+      $('.poster1italic, .poster1, .one').addClass('showme');
     });
 
     $('.poster1').mouseleave(function() {
-   $('.poster1italic, .poster1').removeClass('showme');
+   $('.poster1italic, .poster1, .one').removeClass('showme');
   });
 
 
   $('.poster2').mouseenter(function() {
-      $('.poster2italic, .poster2').addClass('showme');
+      $('.poster2italic, .poster2, .two').addClass('showme');
     });
 
     $('.poster2').mouseleave(function() {
-   $('.poster2italic, .poster2').removeClass('showme');
+   $('.poster2italic, .poster2, .two').removeClass('showme');
   });
 
   $('.poster3').mouseenter(function() {
-      $('.poster3italic, .poster3').addClass('showme');
+      $('.poster3italic, .poster3, .three').addClass('showme');
     });
 
     $('.poster3').mouseleave(function() {
-   $('.poster3italic, .poster3').removeClass('showme');
+   $('.poster3italic, .poster3, .three').removeClass('showme');
   });
 
   $('.poster4').mouseenter(function() {
-      $('.poster4italic, .poster4').addClass('showme');
+      $('.poster4italic, .poster4, .four').addClass('showme');
     });
 
     $('.poster4').mouseleave(function() {
-   $('.poster4italic, .poster4').removeClass('showme');
+   $('.poster4italic, .poster4, .four').removeClass('showme');
   });
 
   $('.poster5').mouseenter(function() {
-      $('.poster5italic, .poster5').addClass('showme');
+      $('.poster5italic, .poster5, .five').addClass('showme');
     });
 
     $('.poster5').mouseleave(function() {
-   $('.poster5italic, .poster5').removeClass('showme');
+   $('.poster5italic, .poster5, .five').removeClass('showme');
   });
 
     });
