6e2d91537d953c25e0680370c5e60c10c02e3471
leannegu <lgu19@student.monash.edu>
2017-11-06 00:52:12 +1100

    time final project

--- Time/assets/js/time.js
+++ Time/assets/js/time.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
   var oddMinute = ' ';
   var halfSecond = ' ';
   var halfMinute = ' ';
+  var halfHour = ' ';
+  var secondhalfHour = ' ';
 
 // Update time
   function update() {
@@ -33,26 +35,30 @@ $(document).ready(function() {
     }
 
     function checkHalfMinute(momentMinute){
-      if(momentMinute < 15) {halfMinute = 'quarterH';}
-      else if(momentMinute < 30){halfMinute = 'halfH';}
-      else if(momentMinute < 45){halfMinute = 'threequarterH';}
+      if(momentMinute < 30) {halfMinute = 'halfH';}
+
+      else if(momentMinute > 29){halfMinute = 'secondhalfH';}
+
       else {halfMinute = 'fullH';}
     }
 
-    // Check if odd, even, quaterpast
+
+    // Check if odd, even, quarterpast
     oddOrEvenS(momentSecond);
     checkHalfSecond(momentSecond);
     oddOrEvenM(momentMinute);
     checkHalfMinute(momentMinute);
 
-    var timeAll = ['s' + momentSecond, 'm' + momentMinute, 'h' + momentHour, momentAMPM, momentDay, momentMonth, 'y' + momentYear, oddSecond, oddMinute, halfSecond, halfMinute];
+
+    var timeAll = ['s' + momentSecond, 'm' + momentMinute, 'h' + momentHour, momentAMPM, momentDay, momentMonth, 'y' + momentYear, oddSecond, oddMinute, halfSecond, halfMinute, halfHour];
     var timeClass = timeAll.join(' ');
 
     // Add classes
     $('body').attr('class', timeClass);
 
     // Add format
-    $('.time').html(moment().format('dddd D MMMM YYYY h:mm:ss:SS A'));
+    $('.time').html(moment().format('h:mm:ss:SS A'));
+    $('.date').html(moment().format('dddd D MMMM YYYY'));
   };
 
   var intervalId = setInterval(update, 100);
